784630c9c096040b4c202fb333e4c8b6d8527755
Jaelly <my106916@outlook.com>
2018-04-23 20:59:20 +0800

    build the first server

--- lesson0/app.js
+++ lesson0/app.js
@@ -1,1 +1,10 @@
-alert(11);
+let express = require('express');
+let app = express();
+
+app.get('/',function (req,res) {
+    res.send('hellow world');
+});
+//注意此处是listen
+app.listen('3000',function () {
+    console.log('listen adress: http://localhost:3000');
+});
